147e6256a6155f15de64960ea7c91dcfba9b3a22
mayuribalapure026 <mayuribalapure002@gmail.com>
2024-09-28 17:59:36 +0530

    Created Header Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import DashBoard from "./pages/DashBoard"
 import Projects from "./pages/Projects"
+import Header from "./components/Header"
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
@@ -16,7 +18,6 @@ function App() {
       <Route path="/signup" element={<SignUp/>}/>
       <Route path="/dashboard" element={<DashBoard/>}/>
       <Route path="/projects" element={<Projects/>}/>
-      <Route path="/home" element={<Home/>}/>
     </Routes>
     </BrowserRouter>
   )
